4dbd05da57c3536bd406b027f18fb90729b592cf
KBDRT <midaskbadrat@gmail.com>
2025-07-02 13:37:47 +0300

    Folders

---
+++ EducationProcess/Backend/src/Presentation/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Application.Exceptions;
+
+namespace Presentation.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try 
+            {
+                await _next.Invoke(context); 
+            }
+            catch (ValidatorFactoryException ex) 
+            {
+                _logger.LogError($"{ex.Message}, Unknown validator for type: {ex.ValidatorType?.ToString()}, Sourse: {ex.Source}, MethodName: {ex.TargetSite}");
+                _logger.LogError($" STACK_TRACE: \n {ex}");
+
+                context.Response.StatusCode = 500;
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError($"EXCEPTION: {ex.Message}, Sourse: {ex.Source}, MethodName: {ex.TargetSite}");
+                _logger.LogError($" STACK_TRACE: \n {ex}");
+
+                context.Response.StatusCode = 500;
+            }
+        }
+    }
+}
---
+++ EducationProcess/Backend/src/Presentation/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Presentation.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            HttpRequest? request = context.Request; 
+            _logger.LogInformation($"REQUEST: {request.Path}, {request.Method}");
+
+            await _next.Invoke(context);
+
+            //_logger.LogDebug($" REQUEST RESULT: {request.Path}, {request.Method}, {context.Response.StatusCode}");
+
+        }
+
+    }
+}
